8f6655837fa0dc869044ade58dd19f4160e727fd
Roland Lötscher <roland_loetscher@hotmail.com>
2025-03-26 05:20:16 +0100

    Buchstaben hinzufügen

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ flow_box = workbench.builder.get_object("flow_box")
 word = workbench.builder.get_object("word")
 overlay = workbench.builder.get_object("overlay")
 drawing_area = workbench.builder.get_object("drawing_area")
+
+def append_letters():
+    for ascii in range(65, 65 + 26):
+        letter = chr(ascii)
+        button = Gtk.Button(label=letter)
+        button.add_css_class("pill")
+        button.add_css_class("circular")
+        flow_box.append(button)
+
+append_letters()
